3c0ad59ffae6c2edcda818a65ebc4403b1fae469
Robert Coomber <robertnc@gmail.com>
2022-01-13 18:09:11 -0800

    Update to the code

--- Sources/RTASLibrary/RTAS.swift
+++ Sources/RTASLibrary/RTAS.swift
@@ -7,8 +7,17 @@
 
 import Foundation
 
-func PrintAllThree(){
+public class RTASClass
+{
+    public func DoTheThing()
+    {
+        print("The Thing is happening")
+    }
+}
+
+public func PrintAllThree(){
     PrintRobert()
     PrintHelloWorld()
     PrintAppleProducts()
 }
+
